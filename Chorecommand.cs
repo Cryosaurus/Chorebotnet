e9c8f27fbd0e9257321c75072a92b9760a70b892
Schneeraubtier <snowraptor@live.ca>
2017-10-18 14:55:07 -0600

    Basic bot operational

---
+++ Chorecommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace Chorebotnet {
+    public class Chorecommand {
+
+        [Command("commands")]
+        public async Task Commands(CommandContext context) {
+            await context.RespondAsync($"I currently do nothing");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+
+namespace Chorebotnet{
+    class Chorecore{
+
+        static DiscordClient discord;
+        static CommandsNextModule commands;
+
+        static void Main(string[] args){
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args) {
+            discord = new DiscordClient(new DiscordConfiguration {
+                Token = args[0],
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration {
+                StringPrefix = "!"
+            });
+
+            commands.RegisterCommands<Chorecommand>();
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
